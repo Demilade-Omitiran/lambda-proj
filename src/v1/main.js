a70a10f2ccd3fe225ad2fa8eafe6e7d38cd012e0
Demilade Omitiran <omitirand@gmail.com>
2024-05-21 14:27:47 +0100

    Init project

---
+++ src/v1/main.js
@@ -0,0 +1,33 @@
+import express, { json, urlencoded } from "express";
+import "dotenv/config";
+import cors from "cors";
+import helmet from "helmet";
+import ServerlessHttp from "serverless-http";
+import routes from "./routes.js";
+
+const { PORT } = process.env;
+
+function initApp() {
+  const app = express();
+
+  app.use(cors());
+  app.use(helmet());
+  app.use(json());
+  app.use(urlencoded({ extended: false }));
+
+  app.use("/api/v1/", routes);
+
+  return app;
+}
+
+function start() {
+  const app = initApp();
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+
+  return ServerlessHttp(app);
+}
+
+export default start();
---
+++ src/v1/routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/hello", (req, res) => res.status(200).json({
+  message: "Sessions-Assessment-V1 API",
+}));
+
+router.get("/health-check", (req, res) => res.status(200).json({
+  uptime: process.uptime(),
+  responsetime: process.hrtime(),
+  message: 'OK',
+  timestamp: Date.now(),
+}));
+
+router.all("*", (req, res) => res.status(404).json({
+  error: "Path not found",
+}));
+
+router.use((error, req, res, next) => {
+  console.log(error);
+  return res.status(500).json({
+    status: "error",
+    message: "Something went wrong",
+  });
+});
+
+export default router;
