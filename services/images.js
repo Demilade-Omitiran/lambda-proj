1eb70c31c82d48065d21395bc6d72af63e2d806e
Demilade Omitiran <omitirand@gmail.com>
2024-05-22 19:00:04 +0100

    Create Images service

---
+++ services/images.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import { throwApiError } from "../../helpers/throw_api_error";
+
+class ImagesService {
+
+}
+
+export default ImagesService;
