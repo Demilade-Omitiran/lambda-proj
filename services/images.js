44bc105e596e9c8a49eb30f5e54eea2c6614f3ba
Demilade Omitiran <omitirand@gmail.com>
2024-05-23 15:51:20 +0100

    Update downloadFile method

--- services/images.js
+++ services/images.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import * as stream from 'stream';
+import { promisify } from 'util';
 import { v4 as uuidv4 } from "uuid";
 import { throwApiError } from "../../helpers/throw_api_error";
 import { sequelize } from "../db/models/index.cjs";
@@ -6,19 +8,26 @@ import { sequelize } from "../db/models/index.cjs";
 class ImagesService {
   static async downloadFile(url) {
     try {
+      const finished = promisify(stream.finished);
+      const writer = createWriteStream(`/temp/${fileName}`);
+
       const res = await axios({
-        method: "get",
+        method: 'get',
         url,
-        responseType: "stream"
+        responseType: 'stream',
       });
 
+      response.data.pipe(writer);
       const fileName = uuidv4();
-
-      res.data.pipe(fs.createWriteStream(`/temp/${fileName}`));
+      await finished(writer);
       return fileName;
     } catch (error) {
       throwApiError("Failed to download image");
     }
+  }
+
+  static async saveImageToS3Bucket(name, fileName) {
+
   }
 
   static async saveImageToDatabase(name, s3Url) {
