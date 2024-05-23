4152d604b99c413997325dfa2b8b0afc38dc60f9
Demilade Omitiran <omitirand@gmail.com>
2024-05-23 13:45:21 +0100

    Create downloadFile method

--- services/images.js
+++ services/images.js
@@ -3,7 +3,22 @@ import { v4 as uuidv4 } from "uuid";
 import { throwApiError } from "../../helpers/throw_api_error";
 
 class ImagesService {
+  static async downloadFile(url) {
+    try {
+      const res = await axios({
+        method: "get",
+        url,
+        responseType: "stream"
+      });
 
+      const fileName = uuidv4();
+
+      res.data.pipe(fs.createWriteStream(`/temp/${fileName}`));
+      return fileName;
+    } catch (error) {
+      throwApiError("Failed to download image");
+    }
+  }
 }
 
 export default ImagesService;
