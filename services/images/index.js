4edc711c4156a8396c8db37b5f166bd14d717c4f
Demilade Omitiran <omitirand@gmail.com>
2024-05-23 21:12:41 +0100

    Export getImageFromDatabase method from Images service.

--- services/images/index.js
+++ services/images/index.js
@@ -1,7 +1,9 @@
 import saveImage from "./save_image.js";
+import getImageFromDatabase from "./get_image_from_database.js";
 
 const ImagesService = {
   saveImage,
+  getImageFromDatabase
 };
 
 export default ImagesService;
