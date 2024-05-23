ae7a6aea1799e6f2003ec5cfa427c5fe85706b8d
Demilade Omitiran <omitirand@gmail.com>
2024-05-23 21:13:18 +0100

    Create GET /:name endpoint

--- routes/images.js
+++ routes/images.js
@@ -4,5 +4,6 @@ import ImagesController from "../controllers/images.js";
 const router = Router();
 
 router.post("/", ImagesController.saveImage);
+router.get("/:name", ImagesController.getImageByName);
 
 export default router;
