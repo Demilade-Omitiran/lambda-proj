6bc686feede50e10d346046f2d9b5854141a6263
Demilade Omitiran <omitirand@gmail.com>
2024-05-24 00:42:37 +0100

    Fix path error

--- services/images/download_file.js
+++ services/images/download_file.js
@@ -1,11 +1,16 @@
 import * as stream from 'stream';
 import { promisify } from 'util';
 import fs from "fs";
+import { fileURLToPath } from 'url';
+import path from "path";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { throwApiError } from "../../helpers/throw_api_error.js";
 
 const IMAGE_EXTENSIONS = ["jpg", "jpeg", "png", "gif", "tiff"];
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function downloadFile(url, fileName = null) {
   const parsedUrl = new URL(url);
@@ -18,7 +23,7 @@ async function downloadFile(url, fileName = null) {
   try {
     const finished = promisify(stream.finished);
     fileName = fileName || `${uuidv4()}.${extension}`;
-    const writer = fs.createWriteStream(`./temp/${fileName}`);
+    const writer = fs.createWriteStream(path.join(__dirname, `../../temp/${fileName}`));
 
     const res = await axios({
       method: 'get',
