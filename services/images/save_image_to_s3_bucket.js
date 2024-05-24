79cc55a92597ca5504cd88d7ce836455298e2629
Demilade Omitiran <omitirand@gmail.com>
2024-05-24 01:45:01 +0100

    Update env variable name

--- services/images/save_image_to_s3_bucket.js
+++ services/images/save_image_to_s3_bucket.js
@@ -6,7 +6,7 @@ import { promisify } from 'util';
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { throwApiError } from "../../helpers/throw_api_error.js";
 
-const { AWS_S3_BUCKET, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
+const { AWS_S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,7 +16,7 @@ async function saveImageToS3Bucket(fileName) {
     const readFileAsync = promisify(fs.readFile);
     const client = new S3Client({
       credentials: {
-        accessKeyId: AWS_ACCESS_KEY,
+        accessKeyId: AWS_ACCESS_KEY_ID,
         secretAccessKey: AWS_SECRET_ACCESS_KEY,
       },
       region: AWS_REGION
@@ -33,14 +33,6 @@ async function saveImageToS3Bucket(fileName) {
     return `https://${AWS_S3_BUCKET}.s3.amazonaws.com/${fileName}`;
   } catch (error) {
     console.log(error);
-    console.log("AWS_S3_BUCKET");
-    console.log(AWS_S3_BUCKET);
-    console.log("AWS_ACCESS_KEY");
-    console.log(AWS_ACCESS_KEY);
-    console.log("AWS_SECRET_ACCESS_KEY");
-    console.log(AWS_SECRET_ACCESS_KEY);
-    console.log("AWS_REGION");
-    console.log(AWS_REGION);
     throwApiError(400, "Failed to upload image to S3");
   }
 }
