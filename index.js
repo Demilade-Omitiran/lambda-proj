7cb1b041f21c87948133ce8a27573965c1a59cd7
Demilade Omitiran <omitirand@gmail.com>
2024-05-24 01:21:21 +0100

    Create tmp folder

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express, { json, urlencoded } from "express";
 import "dotenv/config";
 import cors from "cors";
 import helmet from "helmet";
+import fs from "fs";
+import { promisify } from 'util';
 import ServerlessHttp from "serverless-http";
 import routes from "./routes/index.js";
 
@@ -20,8 +22,23 @@ function initApp() {
   return app;
 }
 
+function createTmpFolder() {
+  const dir = './tmp';
+
+  const fsAccess = promisify(fs.access);
+  const fsMkdir = promisify(fs.mkdir);
+
+  fsAccess(dir, fs.constants.F_OK)
+    .catch((err1) => {
+      fsMkdir(dir);
+    }).catch((err2) => {
+      throw err2;
+    });
+}
+
 function start() {
   const app = initApp();
+  createTmpFolder();
 
   if (ENVIRONMENT !== "lambda") {
     app.listen(PORT, () => {
